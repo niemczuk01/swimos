a8ad244187499939b5e4c1f27b44b1df66b655e6
niemczuk01 <niemczuk01@gmail.com>
2026-03-18 11:42:33 -0400

    add force-dynamic to workouts page

--- app/workouts/page.tsx
+++ app/workouts/page.tsx
@@ -1,5 +1,7 @@
 import { supabase } from '../lib/supabase'
 
+export const dynamic = 'force-dynamic'
+
 export default async function WorkoutsPage() {
   const { data: workouts, error } = await supabase
     .from('workouts')
